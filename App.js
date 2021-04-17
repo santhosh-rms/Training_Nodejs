89d133cf295d956fca35c3daf5a920bf72395a9d
Dona <dona@squashapps.com>
2021-04-17 16:54:24 +0530

    sample req and res

---
+++ App.js
@@ -0,0 +1,32 @@
+// var http =require('http');
+// http.createServer(function(req,res){
+//     res.writeHead(200,{'Content-type':'text/plain'});
+//     res.write("Welcome All");
+//     res.end("Thank You")
+// }).listen(5000);
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log('INCOMING REQUEST');
+  console.log(req.method, req.url);
+
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('end', () => {
+      const userName = body.split('=')[1];
+      res.end('<h1>' + userName + '</h1>');
+    });
+
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+  } else {
+    res.setHeader('Content-Type', 'text/html');
+    res.end(
+      '<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
+    );
+  }
+});
+
+server.listen(5000);
